de5b3da891c7f90a4dd6015456aac4086ff23af5
Euripide Carpio <111103974+Euripidec@users.noreply.github.com>
2024-06-30 15:12:41 -0400

    first commit

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeSelectorComponent } from '../../components/home-selector/home-selector.component';
 import { ScrollIconComponent } from '../../components/scroll-icon/scroll-icon.component';
+import { ShareButtonComponent } from '../../components/share-button/share-button.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HomeSelectorComponent, ScrollIconComponent],
+  imports: [HomeSelectorComponent, ScrollIconComponent,ShareButtonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
