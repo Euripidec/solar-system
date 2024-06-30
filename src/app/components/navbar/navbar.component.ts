ee7560ad170c1913ff20096dfc622b7f6c754f73
Euripide Carpio <111103974+Euripidec@users.noreply.github.com>
2024-06-30 04:49:36 -0400

    first commit

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,6 +8,33 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, AfterViewInit {
+
+  constructor(private elRef: ElementRef) { }
+
+  ngOnInit(): void {
+    // Initialize any component-specific logic here if needed
+  }
+
+  ngAfterViewInit(): void {
+    // jQuery code wrapped in Angular lifecycle hook
+    $(this.elRef.nativeElement).find('.hamwrap').on('click', (e) => {
+      e.preventDefault();
+      $('.navbar-links ul').toggleClass('navbar-links_active');
+      $('.first, .second, .third').toggleClass('f s t');
+      $('.navbar-links').toggleClass('navbar-links_expanded');
+    });
+
+    $('.navbar-links').on('click', () => {
+      $('.hamwrap').click();
+    });
+
+    $(window).on('scroll', () => {
+      $('.navbar-links ul').removeClass('navbar-links_active');
+      $('.hamwrap').removeClass('hamwrap_active');
+      $('.first, .second, .third').removeClass('f s t');
+      $('.navbar-links').removeClass('navbar-links_expanded');
+    });
+  }
 
 }
